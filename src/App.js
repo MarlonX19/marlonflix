0773b5d99f3e326c4bd313c6761df6e30a3bbb6b
marlonx19 <marlon.englemam@gmail.com>
2020-07-28 10:16:54 -0300

    added carousels

--- src/App.js
+++ src/App.js
@@ -11,14 +11,22 @@ function App() {
   return (
     <div>
       <Menu />
-      <BannerMain 
+      <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
         videoDescription={'React Navigation V'}
       />
-      <Carousel 
+      <Carousel
         ignoreFirstVideo
         category={dadosIniciais.categorias[0]}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[1]}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[2]}
       />
       <Footer />
     </div>
